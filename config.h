263c28e98a37c61a80f9ed1b34494decf17c5e86
jmainhard <jmainhard@outlook.com>
2023-10-18 20:53:44 -0300

    port a cliente thingsboard

--- config.h
+++ config.h
@@ -1,13 +1,32 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-// secrets and config
-const char* ssid = "SSID";
-const char* password = "PASSWORD";
-const char* thingsboardServer = "TB_SERVER";
-const int thingsboardPort = 1883; // Puerto predeterminado de MQTT
+#define THINGSBOARD_ENABLE_PSRAM 0
+#define THINGSBOARD_ENABLE_DYNAMIC 1
 
-// Token de acceso de ThingsBoard, generado en la plataforma
-const char* token = "DEVICE_TOKEN";
+#ifndef LED_BUILTIN
+#define LED_BUILTIN 6
+#endif
+
+// secrets and config
+constexpr char WIFI_SSID[] = "SSID";
+constexpr char WIFI_PASSWORD[] = "PASSWORD";
+
+// See https://thingsboard.io/docs/getting-started-guides/helloworld/
+// to understand how to obtain an access token
+constexpr char TOKEN[] = "TOKEN";
+
+// Thingsboard we want to establish a connection too
+constexpr char THINGSBOARD_SERVER[] = "iot.ceisufro.cl";
+// MQTT port used to communicate with the server, 1883 is the default unencrypted MQTT port.
+constexpr uint16_t THINGSBOARD_PORT = 1883;
+
+// Maximum size packets will ever be sent or received by the underlying MQTT client,
+// if the size is to small messages might not be sent or received messages will be discarded
+constexpr uint32_t MAX_MESSAGE_SIZE = 256U;
+
+// Baud rate for the debugging serial connection.
+// If the Serial output is mangled, ensure to change the monitor speed accordingly to this variable
+constexpr uint32_t SERIAL_DEBUG_BAUD = 9600U;
 
 #endif
--- networking-iot.ino
+++ networking-iot.ino
@@ -1,53 +1,267 @@
 #include <WiFi101.h>
-#include <TBPubSubClient.h>
-
+#include <ThingsBoard.h>
 #include "config.h"
 
+// Initialize underlying client, used to establish a connection
 WiFiClient wifiClient;
-PubSubClient client(wifiClient);
+// Initialize ThingsBoard instance with the maximum needed buffer size
+ThingsBoard tb(wifiClient, MAX_MESSAGE_SIZE);
 
-void setup() {
-  Serial.begin(115200);
-  delay(10);
+// Attribute names for attribute request and attribute updates functionality
+constexpr char BLINKING_INTERVAL_ATTR[] = "blinkingInterval";
+constexpr char LED_MODE_ATTR[] = "ledMode";
+constexpr char LED_STATE_ATTR[] = "ledState";
+int sensor;
 
-  // Conexión WiFi
-  Serial.println();
-  Serial.print("Conectando a: ");
-  Serial.println(ssid);
-  WiFi.begin(ssid, password);
+// Statuses for subscribing to rpc
+bool subscribed = false;
+
+// handle led state and mode changes
+volatile bool attributesChanged = false;
+
+// LED modes: 0 - continious state, 1 - blinking
+volatile int ledMode = 0;
+
+// Current led state
+volatile bool ledState = false;
+
+// Settings for interval in blinking mode
+constexpr uint16_t BLINKING_INTERVAL_MS_MIN = 10U;
+constexpr uint16_t BLINKING_INTERVAL_MS_MAX = 60000U;
+volatile uint16_t blinkingInterval = 1000U;
+
+uint32_t previousStateChange;
+
+// For telemetry
+constexpr int16_t telemetrySendInterval = 2000U;
+uint32_t previousDataSend;
+
+// List of shared attributes for subscribing to their updates
+constexpr std::array<const char *, 2U> SHARED_ATTRIBUTES_LIST = {
+  LED_STATE_ATTR,
+  BLINKING_INTERVAL_ATTR
+};
+
+// List of client attributes for requesting them (Using to initialize device states)
+constexpr std::array<const char *, 1U> CLIENT_ATTRIBUTES_LIST = {
+  LED_MODE_ATTR
+};
+
+/// @brief Initalizes WiFi connection,
+// will endlessly delay until a connection has been successfully established
+void InitWiFi() {
+  Serial.println("Connecting to AP ...");
+  // Attempting to establish a connection to the given WiFi network
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
   while (WiFi.status() != WL_CONNECTED) {
+    // Delay 500ms until a connection has been succesfully established
     delay(500);
     Serial.print(".");
   }
-  Serial.println();
-  Serial.println("Conexión WiFi establecida");
+  Serial.println("Connected to AP");
+}
 
-  client.setServer(thingsboardServer, thingsboardPort);
+/// @brief Reconnects the WiFi uses InitWiFi if the connection has been removed
+/// @return Returns true as soon as a connection has been established again
+const bool reconnect() {
+  if (WiFi.status() == WL_CONNECTED) {
+    return true;
+  }
+
+  // If we aren't establish a new connection to the given WiFi network
+  InitWiFi();
+  return true;
+}
+
+
+/// @brief Processes function for RPC call "setLedMode"
+/// RPC_Data is a JSON variant, that can be queried using operator[]
+/// See https://arduinojson.org/v5/api/jsonvariant/subscript/ for more details
+/// @param data Data containing the rpc data that was called and its current value
+/// @return Response that should be sent to the cloud. Useful for getMethods
+RPC_Response processSetLedMode(const RPC_Data &data) {
+  Serial.println("Received the set led state RPC method");
+
+  // Process data
+  int new_mode = data;
+
+  Serial.print("Mode to change: ");
+  Serial.println(new_mode);
+
+  if (new_mode != 0 && new_mode != 1) {
+    return RPC_Response("error", "Unknown mode!");
+  }
+
+  ledMode = new_mode;
+
+  attributesChanged = true;
+
+  // Returning current mode
+  return RPC_Response("newMode", (int)ledMode);
+}
+
+
+// Optional, keep subscribed shared attributes empty instead,
+// and the callback will be called for every shared attribute changed on the device,
+// instead of only the one that were entered instead
+const std::array<RPC_Callback, 1U> callbacks = {
+  RPC_Callback{ "setLedMode", processSetLedMode }
+};
+
+
+/// @brief Update callback that will be called as soon as one of the provided shared attributes changes value,
+/// if none are provided we subscribe to any shared attribute change instead
+/// @param data Data containing the shared attributes that were changed and their current value
+void processSharedAttributes(const Shared_Attribute_Data &data) {
+  for (auto it = data.begin(); it != data.end(); ++it) {
+    if (strcmp(it->key().c_str(), BLINKING_INTERVAL_ATTR) == 0) {
+      const uint16_t new_interval = it->value().as<uint16_t>();
+      if (new_interval >= BLINKING_INTERVAL_MS_MIN && new_interval <= BLINKING_INTERVAL_MS_MAX) {
+        blinkingInterval = new_interval;
+        Serial.print("Updated blinking interval to: ");
+        Serial.println(new_interval);
+      }
+    } else if (strcmp(it->key().c_str(), LED_STATE_ATTR) == 0) {
+      ledState = it->value().as<bool>();
+      digitalWrite(LED_BUILTIN, ledState ? HIGH : LOW);
+      Serial.print("Updated state to: ");
+      Serial.println(ledState);
+    }
+  }
+  attributesChanged = true;
+}
+
+void processClientAttributes(const Shared_Attribute_Data &data) {
+  for (auto it = data.begin(); it != data.end(); ++it) {
+    if (strcmp(it->key().c_str(), LED_MODE_ATTR) == 0) {
+      const uint16_t new_mode = it->value().as<uint16_t>();
+      ledMode = new_mode;
+    }
+  }
+}
+
+const Shared_Attribute_Callback attributes_callback(SHARED_ATTRIBUTES_LIST.cbegin(), SHARED_ATTRIBUTES_LIST.cend(), &processSharedAttributes);
+const Attribute_Request_Callback attribute_shared_request_callback(SHARED_ATTRIBUTES_LIST.cbegin(), SHARED_ATTRIBUTES_LIST.cend(), &processSharedAttributes);
+const Attribute_Request_Callback attribute_client_request_callback(CLIENT_ATTRIBUTES_LIST.cbegin(), CLIENT_ATTRIBUTES_LIST.cend(), &processClientAttributes);
+
+// Get the MAC address String
+String macAddress() {
+  byte mac[6];
+  WiFi.macAddress(mac);
+  // Convert MAC address to string
+  String macAddress = "";
+  for (int i = 0; i < 6; i++) {
+    macAddress += String(mac[i], HEX);
+    if (i < 5) {
+      macAddress += ":";
+    }
+  }
+  return macAddress;
+}
+
+
+void setup() {
+  // Initalize serial connection for debugging
+  Serial.begin(SERIAL_DEBUG_BAUD);
+  pinMode(LED_BUILTIN, OUTPUT);
+  delay(1000);
+  InitWiFi();
 }
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
+  //  delay(10);
+
+  if (!reconnect()) {
+    subscribed = false;
+    return;
   }
 
-  // Simula la lectura de un sensor y envía el valor a ThingsBoard
-  float sensorValue = analogRead(A0) * 5.0 / 1023.0;
-  String payload = "{\"analog_sensor\": " + String(sensorValue, 2) + "}";
-  client.publish("v1/devices/me/telemetry", payload.c_str());
+  if (!tb.connected()) {
+    subscribed = false;
+    // Connect to the ThingsBoard
+    Serial.print("Connecting to: ");
+    Serial.print(THINGSBOARD_SERVER);
+    Serial.print(" with token ");
+    Serial.println(TOKEN);
+    if (!tb.connect(THINGSBOARD_SERVER, TOKEN, THINGSBOARD_PORT)) {
+      Serial.println("Failed to connect");
+      return;
+    }
 
-  delay(10000); // Envía datos cada 10 segundos (ajustable)
-}
+    // Send the MAC address as an attribute
+    tb.sendAttributeData("macAddress", macAddress().c_str());
+  }
 
-void reconnect() {
-  while (!client.connected()) {
-    Serial.print("Conectando a ThingsBoard...");
-    if (client.connect("ArduinoMKR1000", token, NULL)) {
-      Serial.println("Conectado");
-    } else {
-      Serial.print("falló, rc=");
-      Serial.print(client.state());
-      Serial.println(" Intentando de nuevo en 5 segundos");
-      delay(5000);
+  if (!subscribed) {
+    Serial.println("Subscribing for RPC...");
+    // Perform a subscription. All consequent data processing will happen in
+    // processSetLedState() and processSetLedMode() functions,
+    // as denoted by callbacks array.
+    if (!tb.RPC_Subscribe(callbacks.cbegin(), callbacks.cend())) {
+      Serial.println("Failed to subscribe for RPC");
+      return;
+    }
+
+    if (!tb.Shared_Attributes_Subscribe(attributes_callback)) {
+      Serial.println("Failed to subscribe for shared attribute updates");
+      return;
+    }
+
+    Serial.println("Subscribe done");
+    subscribed = true;
+
+    // Request current states of shared attributes
+    if (!tb.Shared_Attributes_Request(attribute_shared_request_callback)) {
+      Serial.println("Failed to request for shared attributes");
+      return;
+    }
+
+    // Request current states of client attributes
+    if (!tb.Client_Attributes_Request(attribute_client_request_callback)) {
+      Serial.println("Failed to request for client attributes");
+      return;
     }
   }
+
+  if (attributesChanged) {
+    attributesChanged = false;
+    if (ledMode == 0) {
+      previousStateChange = millis();
+    }
+    tb.sendTelemetryData(LED_MODE_ATTR, ledMode);
+    tb.sendTelemetryData(LED_STATE_ATTR, ledState);
+    tb.sendAttributeData(LED_MODE_ATTR, ledMode);
+    tb.sendAttributeData(LED_STATE_ATTR, ledState);
+  }
+
+  if (ledMode == 1 && millis() - previousStateChange > blinkingInterval) {
+    previousStateChange = millis();
+    ledState = !ledState;
+    digitalWrite(LED_BUILTIN, ledState);
+    tb.sendTelemetryData(LED_STATE_ATTR, ledState);
+    tb.sendAttributeData(LED_STATE_ATTR, ledState);
+    if (LED_BUILTIN == 99) {
+      Serial.print("LED state changed to: ");
+      Serial.println(ledState);
+    }
+  }
+
+  // Sending telemetry every telemetrySendInterval time
+  if (millis() - previousDataSend > telemetrySendInterval) {
+    previousDataSend = millis();
+    // tb.sendTelemetryData("temperature", 1l);
+  }
+
+  sensor = analogRead(A1) - 130;
+  Serial.print("sensor: ");
+  Serial.println(sensor);
+  if (sensor >= 250) {
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    tone(A2, 4500, 100);
+    delay(2000);
+    digitalWrite(LED_BUILTIN, LOW);
+  }
+
+  tb.loop();
 }
